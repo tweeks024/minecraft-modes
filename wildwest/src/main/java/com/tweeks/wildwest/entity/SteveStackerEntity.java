ef15bab9b96168753f262d53b3b5b0afc53f8533
Tom Weeks <tweeks@Toms-MacBook-Pro.local>
2026-05-08 14:45:51 -0400

    feat(wildwest): SteveStacker synced STACK_HEIGHT data accessor + NBT

--- wildwest/src/main/java/com/tweeks/wildwest/entity/SteveStackerEntity.java
+++ wildwest/src/main/java/com/tweeks/wildwest/entity/SteveStackerEntity.java
@@ -1,5 +1,8 @@
 package com.tweeks.wildwest.entity;
 
+import net.minecraft.network.syncher.EntityDataAccessor;
+import net.minecraft.network.syncher.EntityDataSerializers;
+import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
@@ -13,8 +16,13 @@ import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
 import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.storage.ValueInput;
+import net.minecraft.world.level.storage.ValueOutput;
 
 public class SteveStackerEntity extends Monster {
+
+    private static final EntityDataAccessor<Byte> STACK_HEIGHT =
+        SynchedEntityData.defineId(SteveStackerEntity.class, EntityDataSerializers.BYTE);
 
     public SteveStackerEntity(EntityType<? extends SteveStackerEntity> type, Level level) {
         super(type, level);
@@ -31,6 +39,20 @@ public class SteveStackerEntity extends Monster {
     }
 
     @Override
+    protected void defineSynchedData(SynchedEntityData.Builder builder) {
+        super.defineSynchedData(builder);
+        builder.define(STACK_HEIGHT, (byte) 3);
+    }
+
+    public byte getStackHeight() {
+        return this.entityData.get(STACK_HEIGHT);
+    }
+
+    private void setStackHeight(byte value) {
+        this.entityData.set(STACK_HEIGHT, value);
+    }
+
+    @Override
     protected void registerGoals() {
         this.goalSelector.addGoal(0, new FloatGoal(this));
         this.goalSelector.addGoal(1, new MeleeAttackGoal(this, 1.0, true));
@@ -40,5 +62,17 @@ public class SteveStackerEntity extends Monster {
 
         this.targetSelector.addGoal(1, new HurtByTargetGoal(this));
         this.targetSelector.addGoal(2, new NearestAttackableTargetGoal<>(this, Player.class, true));
+    }
+
+    @Override
+    protected void addAdditionalSaveData(ValueOutput output) {
+        super.addAdditionalSaveData(output);
+        output.putByte("StackHeight", this.getStackHeight());
+    }
+
+    @Override
+    protected void readAdditionalSaveData(ValueInput input) {
+        super.readAdditionalSaveData(input);
+        this.setStackHeight(input.getByteOr("StackHeight", (byte) 3));
     }
 }
